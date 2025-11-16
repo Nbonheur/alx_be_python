dd98575fccecf9575e24e461b623fc158647a691
Bonheur_Ndamukuna <ndamukundabonheur@gmail.com>
2025-11-16 22:41:44 +0100

    made some changes that were missing in match_case_calculator

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -13,7 +13,10 @@ match operator:
         result = num1 * num2
         print(f"The result is {result}.")
     case "/":
-        result = num1 / num2
-        print(f"The resut is {result}.")
+        if num2 == 0:
+            print("Cannot divide by zero.")
+        else:
+            result = num1 / num2
+            print(f"The resut is {result}.")
     case _:
         print("This operation doesn't exist.")
