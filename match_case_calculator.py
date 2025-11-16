8e068e9a6d3c40c0c1420d72bd55afe93b06421f
Bonheur_Ndamukuna <ndamukundabonheur@gmail.com>
2025-11-16 22:13:28 +0100

    created calculator using match case statements

---
+++ match_case_calculator.py
@@ -0,0 +1,19 @@
+num1 = int(input("Enter the first numer: "))
+num2 = int(input("Enter the second number: "))
+operator = input("Choose the operation (+, -, *, /): ")
+
+match operator:
+    case "+":
+        result = num1 + num2
+        print(f"The result is {result}.")
+    case "-":
+        result = num1 - num2
+        print(f"The rsult is {result}.")
+    case "*":
+        result = num1 * num2
+        print(f"The result is {result}.")
+    case "/":
+        result = num1 / num2
+        print(f"The resut is {result}.")
+    case _:
+        print("This operation doesn't exist.")
