ce516e0bc6c56b7a53ef6b0fd3f06ccd7ab3262a
Nbonheur <77569346+Nbonheur@users.noreply.github.com>
2025-11-16 23:13:56 +0100

    Fix typo in print statement for division result

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -17,6 +17,6 @@ match operation:
             print("Cannot divide by zero.")
         else:
             result = num1 / num2
-            print(f"The resut is {result}.")
+            print(f"The result is {result}.")
     case _:
         print("This operation doesn't exist.")
