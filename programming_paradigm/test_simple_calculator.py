098f9c17ca77e225f5978321e287e97306368015
Nbonheur <ndamukundabonheur@gmail.com>
2025-11-29 17:54:39 +0100

    modified simple_calculator.py and made some changes on test_simple_calculator

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -5,63 +5,33 @@ class TestSimpleCalculator(unittest.TestCase):
     def setUp(self):
         self.calc = SimpleCalculator()
 
-    def test_addition_with_integers(self):
+    def test_addition(self):
         self.assertEqual(self.calc.add(2, 3), 5)
         self.assertEqual(self.calc.add(-1, 1), 0)
         self.assertEqual(self.calc.add(0, 0), 0)
-
-    def test_addition_with_floats(self):
+        self.assertEqual(self.calc.add(100, 200), 300)
         self.assertAlmostEqual(self.calc.add(2.5, 1.2), 3.7, places=7)
-        self.assertAlmostEqual(self.calc.add(-1.1, -2.2), -3.3, places=7)
 
-    def test_addition_with_large_numbers(self):
-        self.assertEqual(self.calc.add(10**12, 10**12), 2 * 10**12)
-
-    def test_subtraction_with_integers(self):
+    def test_subtraction(self):
         self.assertEqual(self.calc.subtract(10, 4), 6)
         self.assertEqual(self.calc.subtract(-1, -1), 0)
         self.assertEqual(self.calc.subtract(0, 5), -5)
-
-    def test_subtraction_with_floats(self):
         self.assertAlmostEqual(self.calc.subtract(5.5, 2.2), 3.3, places=7)
-        self.assertAlmostEqual(self.calc.subtract(-1.5, 1.5), -3.0, places=7)
 
-    def test_multiplication_with_integers(self):
+    def test_multiply(self):
         self.assertEqual(self.calc.multiply(3, 4), 12)
         self.assertEqual(self.calc.multiply(-2, 5), -10)
-        self.assertEqual(self.calc.multiply(0, 100), 0)
-
-    def test_multiplication_with_floats(self):
+        self.assertEqual(self.calc.multiply(-3, -3), 9)
         self.assertAlmostEqual(self.calc.multiply(2.5, 2), 5.0, places=7)
-        self.assertAlmostEqual(self.calc.multiply(-1.5, -2), 3.0, places=7)
 
-    def test_multiplication_with_large_numbers(self):
-        self.assertEqual(self.calc.multiply(10**6, 10**3), 10**9)
-
-    def test_division_normal(self):
+    def test_divide(self):
         self.assertEqual(self.calc.divide(10, 2), 5.0)
         self.assertAlmostEqual(self.calc.divide(7, 2), 3.5, places=7)
         self.assertAlmostEqual(self.calc.divide(-9, 3), -3.0, places=7)
-
-    def test_division_with_floats(self):
-        self.assertAlmostEqual(self.calc.divide(2.5, 0.5), 5.0, places=7)
-        self.assertAlmostEqual(self.calc.divide(-1.5, 0.5), -3.0, places=7)
-
-    def test_divide_by_zero_returns_none(self):
+        self.assertAlmostEqual(self.calc.divide(1, 3), 1.0/3.0, places=7)
         self.assertIsNone(self.calc.divide(10, 0))
         self.assertIsNone(self.calc.divide(0, 0))
 
-    def test_division_edge_cases(self):
-        # division that results in a repeating float
-        result = self.calc.divide(1, 3)
-        self.assertIsInstance(result, float)
-        self.assertAlmostEqual(result, 1.0 / 3.0, places=7)
-
-    def test_operations_with_mixed_types(self):
-        self.assertAlmostEqual(self.calc.add(1, 2.5), 3.5, places=7)
-        self.assertAlmostEqual(self.calc.multiply(3, 2.5), 7.5, places=7)
-        self.assertAlmostEqual(self.calc.subtract(5.5, 2), 3.5, places=7)
-        self.assertAlmostEqual(self.calc.divide(5, 2.0), 2.5, places=7)
-
 if __name__ == "__main__":
     unittest.main()
+
